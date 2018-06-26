14d2611dd203d5e4a0b7f702de595294fe900ee9
Trevor Osborne <tosborne@csumb.edu>
2018-06-26 02:49:54 +0000

    HW2

---
+++ hw/homework2/inc/functions.php
@@ -0,0 +1,68 @@
+<?php
+    
+    /*
+     *The main() function
+     *Chooses weapons randomly for players
+     */
+     function play() {
+         
+         for ($i=1; $i<4; $i++){
+             ${"randomValue" . $i} = rand(0,7);
+             displaySymbol(${"randomValue" . $i}, $i);
+         }
+         displayPoints($randomValue1, $randomValue2, $randomValue3);
+     }
+    
+    
+     /*
+    *Takes a random number parameter
+    *Displays a corresponding game symbol
+    **/
+    function displaySymbol($randomValue, $pos) {
+                switch ($randomValue) {
+                    case 0: $symbol = "dissappeares";
+                        break;
+                    case 1: $symbol = "teddybear";
+                        break;
+                    case 2: $symbol = "sniperrifle";
+                        break;
+                    case 3: $symbol = "dualpistols";
+                        break;
+                    case 4: $symbol = "cymbalsmonkey";
+                        break;
+                    case 5: $symbol = "chinalake";
+                        break;
+                    case 6: $symbol = "ak47";
+                        break;
+                    case 7: $symbol = "alienblaster";
+                }
+                echo "<img id = 'reel$pos' src='img/$symbol.jpeg' alt='$symbol' title='". ucfirst($symbol) . "' width='70' />";
+    }
+            
+    /*
+     *Displays win/loss based off worthiness of guns in arsenal
+     *
+    **/
+    function displayPoints($randomValue1, $randomValue2, $randomValue3) {
+        
+        echo "<div id='output'>";
+        $arsenal = array($randomValue1, $randomValue2, $randomValue3);
+        for ($p = 0; $p < count($arsenal); $p++) {
+            if ("randomValue" . $p == 7) {
+                 rsort($arsenal);
+                 echo "<h2>Alien Blaster! Kindly, you trade guns \n with your pal 
+                 who has never \n experienced alien blaster carnage \n before.</h2>";
+                 $p = 3;
+                 break;
+            }
+        }
+        if (array_sum($arsenal) <= 7) {
+            echo nl2br("<h3>Not enough firepower.\n\nYour team was slaughtered by\n\nthe zombie horde.</h3>");
+        } else if (array_sum($arsenal) <= 15) {
+            echo nl2br("<h3>The box was unpredictable.\n\nBoth teammates are\n\ndown. You're on your own...</h3>"); 
+        } else {
+            echo nl2br("<h3>The box has been gracious. \n\n Bring on the zombies!</h3>");
+        }
+        echo "</div>";
+    }
+?>
---
+++ hw/homework2/index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<?php
+    include 'inc/functions.php';
+?>
+<!--
+Homework 2: Array and Loops
+-->
+<html>
+    <head>
+        <title> Call of Duty: Mystery Box </title>
+        <style>
+            @import url("css/styles.css");
+        </style>
+        <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    </head>
+    <body>
+         <header>
+            <h1> Call of Duty -BlackOps2- Mystery Box </h1>
+        </header>
+        
+        <br /><br />
+        
+        <main>
+        
+        <figure id="box">
+            <img src="img/mysteryBox.jpg" alt="Picture of Mystery Box" />
+        </figure>
+        
+            <div id="main">   
+               <?php  
+                play();
+               ?> 
+          
+              <form>
+                  <input type="Submit" vlue="Open"/>
+              </form>
+            </div>
+        
+        </main>
+        
+        <footer>
+            <br /><br />
+            <br /><br />
+            <br /><br />
+            <br /><br />
+            You're fighting against endless zombies and have finally found the mystery box.
+            It costs quite a bit of cash to chance the box, but you've been severing zombie heads with your knife
+            and saving up. Time to equip your team of three with the firepower you'll need
+            to survive.
+        </footer>
+        <!-- closing footer -->
+    </body>
+</html>
