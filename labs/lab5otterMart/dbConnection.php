f18d428a1e41b87a662c9db117843950ab806de5
Trevor Osborne <tosborne@csumb.edu>
2018-07-10 16:56:03 +0000

    added tables to Heroku db lab 5 otterMart

--- labs/lab5otterMart/dbConnection.php
+++ labs/lab5otterMart/dbConnection.php
@@ -2,10 +2,21 @@
 
 function getDatabaseConnection($dbname = 'ottermart') {
     
-    $host = 'localhost';//cloud 9
+    //cloud 9 db info
+    $host = 'localhost';
     //$dbname = 'tcp';
     $username = 'root';
     $password = '';
+    
+    //when connecting from Heroku
+    if  (strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
+        $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+        $host = $url["host"];
+        $dbname = substr($url["path"], 1);
+        $username = $url["user"];
+        $password = $url["pass"];
+    } 
+
     
     //creates db connection
     $dbConn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
@@ -15,13 +26,5 @@ function getDatabaseConnection($dbname = 'ottermart') {
     
     return $dbConn;
 }
-
-
-
-
-
-
-
-
 
 ?>
